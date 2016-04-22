728471d2fcf443aeb664ad9044c345dfad2f0765
Samuel Green <green.sam1192@gmail.com>
2016-04-22 13:05:31 -0400

    add sentiment

---
+++ Analysis/sentiment.py
@@ -0,0 +1,31 @@
+'''
+@package sentiment
+This code adapts example code from
+Niek Sanders, njs@sananalytics.com,
+originally source here: http://www.sananalytics.com/lab/twitter-sentiment/
+
+It uses the labeled training corpus from that site as
+the training set for the model, though this will be
+at best imperfect, since those tweets were classified
+against topics different from this topic. Should
+be a reasonable baseline anyways. We ignore
+dimensionality reduction for the moment. 
+
+The module assumptions the existence of a 
+training set called 'training.csv'
+'''
+import csv, random
+import nltk
+import tweet_features
+
+def sentiment_classifier():
+	trainingfp = open('training.csv', 'rb')
+	reader = csv.reader( trainingfp, delimiter=',', quotechar='"', escapechar='\\' )
+	trainingtweets = []
+	for row in reader:
+		trainingtweets.append([row[1], row[4]])
+	random.shuffle(trainingtweets)
+
+	fvecs = [(tweet_features.make_tweet_dict(t),s) for (t,s) in trainingtweets]
+	classifier = nltk.NaiveBayesClassifier.train(fvecs);
+	return classifier
