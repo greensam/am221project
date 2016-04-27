1b43fa33021b1d8a24e27bc76d706363132d8cd4
Samuel Green <green.sam1192@gmail.com>
2016-04-26 22:17:10 -0400

    update sentiment classification

--- Analysis/sentiment.py
+++ Analysis/sentiment.py
@@ -24,6 +24,10 @@ import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 
+from sklearn.svm import LinearSVC
+from nltk.classify.scikitlearn import SklearnClassifier
+# SVM with a Linear Kernel and default parameters 
+
 stops = set(stopwords.words("english"))
 
 def tweet_to_words(tweet):
@@ -33,7 +37,12 @@ def tweet_to_words(tweet):
                       twbs.get_text() )
 	words = letters_only.lower().split()
 	meaningful = [w for w in words if not w in stops]
-	return " ".join(meaningful)
+	
+	fset = {}
+	for m in meaningful:
+		fset['contains(%s)' % m.lower()] = True    
+
+	return fset
 
 """
 This method trains a classifier using
@@ -44,7 +53,7 @@ the object that results from this call
 with obj.classify(text) 
 where text is the text of the tweet. 
 """
-def sentiment_classifier():
+def sentiment_classifier(sensitivity=100):
 	# trainingfp = open('training.csv', 'rb')
 	train = pd.read_csv( 'training.csv', delimiter=',', quotechar='"', escapechar='\\',header=0 )
 	num_tweets = train['TweetText'].size
@@ -53,35 +62,40 @@ def sentiment_classifier():
 	for i in xrange(0, num_tweets):
 		if( (i+1)%1000 == 0 ):
 			print "Tweet %d of %d\n" % ( i+1, num_tweets )          
-		cleantweets.append(tweet_to_words(train['TweetText'][i]))
+		cleantweets.append((tweet_to_words(train['TweetText'][i]), train['Sentiment'][i]))
 
-	vectorizer = CountVectorizer(analyzer = "word",   \
-                             tokenizer = None,    \
-                             preprocessor = None, \
-                             stop_words = None,   \
-                             max_features = 5000) 
+	# vectorizer = CountVectorizer(analyzer = "word",   \
+ #                             tokenizer = None,    \
+ #                             preprocessor = None, \
+ #                             stop_words = None,   \
+ #                             max_features = 5000) 
 
-	train_data_features = vectorizer.fit_transform(cleantweets)
+	# train_data_features = vectorizer.fit_transform([t for (t,_) in cleantweets])
+	
+	# feature_labels = [(m,l) for ((f,l),m) in zip(cleantweets, train_data_features)]
 
-	forest = RandomForestClassifier(n_estimators = 100)
-	forest = forest.fit(train_data_features, train['Sentiment'])
+	# forest = RandomForestClassifier(n_estimators = sensitivity)
+	# forest = forest.fit(train_data_features, train['Sentiment'])
+	classif = SklearnClassifier(LinearSVC())
+	classif.train(cleantweets)
 
-	return (forest,vectorizer)
+	return (classif)
 
 class Classifier():
 	
 	def __init__(self):
-		trained =  sentiment_classifier()
-		self.forest = trained[0]
-		self.vectorizer = trained[1]
+		trained = sentiment_classifier()
+	
+		self.forest = trained
+		# self.vectorizer = trained[1]
 
 	def classify(self,tweet):
 		clean = tweet_to_words(tweet)
-		test_features = self.vectorizer.transform([clean])
-		test_features = test_features.toarray()
+		# test_features = self.vectorizer.transform([clean])
+		# test_features = test_features.toarray()
 
-		result = self.forest.predict(test_features)
+		# result = self.forest.predict(test_features)
 
 		# return fvec
-		return result
+		return self.forest.classify(clean)
 
