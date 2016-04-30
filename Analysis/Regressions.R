6290adab6030b49eeb4566407fae379ab88730d0
Samuel Green <green.sam1192@gmail.com>
2016-04-30 18:20:10 -0400

    merge

--- Analysis/Regressions.R
+++ Analysis/Regressions.R
@@ -3,18 +3,18 @@
 ## April 30, 2016
 
 # Import the relevant library for the following section
-install.packages('leaps')
 library(leaps)
 
-install.packages('stargazer')
 library(stargazer)
 
 # Read in the relevant dataset
-sgdf_data0430 = read.csv("~/Dropbox/School/Harvard 15-16/Applied Mathematics 221/Final Project/am221project/Analysis/tweetdata.csv", header=TRUE)
+sgdf_data0430 = read.csv("tweetdata.csv", header=TRUE)
 colnames(sgdf_data0430)
 
 fit1 <- lm(Margin_Period ~ SentDiff_Period + SentDiff_Total + VolDiff_Total + OPA_Margin, data = sgdf_data0430)
 summary(fit1)
+
+fit2 <- lm(Margin_Period ~ )
 
 Margin_TOT
 VolDiff_Period
